169e66de2b720b17e17d6c7bc9cdf52bcccffa29
sanada-h <162587476+sanada-h@users.noreply.github.com>
2024-04-16 10:42:02 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -20,7 +20,8 @@
         $score9 = 30;
         $score10 = 60;
         $sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
-        echo $sum / 10;
+        $average = $sum / 10;
+        echo $average;
         ?>
     </p>
 </body>
